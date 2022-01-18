a66f4fda9c7d91c2428f3ba4935be5c7cdc034fa
MissLiner <miss.liner@yahoo.com>
2022-01-18 14:50:39 -0500

    Finish basic setup

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    mode: 'development',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    watch: true,
+    watchOptions: {
+      aggregateTimeout: 600,
+      ignored: /node_modules/,
+    },
+    module: {
+        rules: [
+        {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+        },
+        {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'asset/resource',
+        },
+        ],
+    },
+};

d9d835baccac58020568b7571892078aaaeae2b5
MissLiner <miss.liner@yahoo.com>
2022-01-26 13:03:19 -0500

    Clean up code

--- src/makeBoard.js
+++ src/makeBoard.js
@@ -16,7 +16,6 @@ const boardFactory = (height, width) => {
     }
   })()
 
-
   function checkForDupes(arr) {
     for(let item of arr) {
       const dupes1 = arr.filter(newItem1 => newItem1.row === item.row);
@@ -25,17 +24,6 @@ const boardFactory = (height, width) => {
     }
     return false;
   }
-  function assignPosition(positionArr) {
-    let position = positionShip(size, direction);
-    const dupeCheckArr = positionArr.concat(position);
-    let isDupe = checkForDupes(dupeCheckArr, 'row', 'column');
-    while(isDupe === true) {
-      position = positionShip(size, direction);
-      const dupeCheckArr2 = positionArr.concat(position);
-      isDupe = checkForDupes(dupeCheckArr2, 'row', 'column');
-    }
-    return position;
-  } 
   function buildArmada(player, armadaArr) {
     const shipLengths = [2, 3, 3, 4, 5];
     let allPositions = [];
@@ -54,75 +42,17 @@ const boardFactory = (height, width) => {
     console.log(armadaArr);
     console.log(allPositions);
   }
-  function checkIfOnBoard(coordinates) {
-    if(coordinates.row > 10) {
-      throw 'ship fell off the bottom edge!'
-    }
-    else if(coordinates.column > 10) {
-      throw 'ship fell off the right side!';
-    }
-  }
-  // function addPositions(ship, positionsArr) {
-  //   let newPositions = [ ship.positions ];
-  //   let isDupe;
 
-  //   function popAllPositions(initPosition) {
-  //     let rowPos = initPosition.row;
-  //     let colPos = initPosition.column;
-      
-  //     for(let i = 0; i < ship.size - 1; i++) {
-  //       let newColumn = colPos + 1 + i;
-  //       let newRow = rowPos + 1 + i;
-  //       let newPosition;
-  //       if(ship.direction === 'horizontal') {
-  //         newPosition = { row: rowPos, column: newColumn }
-  //       } else if(ship.direction === 'vertical') {
-  //         newPosition = { row: newRow, column: colPos }
-  //       }
-  //       checkIfOnBoard(newPosition);
-  //       newPositions.push(newPosition); 
-  //     }
-  //   }
-  //   function checkOverlap() {
-  //     const dupeCheckArr = positionsArr.concat(newPositions);
-  //     isDupe = checkForDupes(dupeCheckArr, 'row', 'column');
-  //   }
-
-  //   popAllPositions(ship.positions);
-  //   checkOverlap();
-
-  //   if(isDupe === true) {
-  //     let flipCounter = 0;
-  //     newPositions.length = 0;
-  //     do {
-  //       if(flipCounter > 5) {
-  //         throw 'I\'m dizzy from too much flipping!!';
-  //       }
-  //       flipDirection(ship);
-  //       flipCounter++;
-  //       console.log(flipCounter);
-  //       const resetPosition = assignPosition(ship.size, ship.direction, positionsArr);
-  //       newPositions.push(resetPosition);
-  //       console.log(newPositions);
-  //       console.log(resetPosition);
-  //       popAllPositions(ship.positions);
-  //       checkOverlap();
-  //     }
-  //     while(isDupe === true); 
-  //   } 
-  // return newPositions;
-  // }
 
   function placeShip(ship, board) {
-        // board.rows[rowPosition][newColumn] = 's';
   }
-  
   function placeArmada(armada, board) {
     for (let ship in armada) {
       placeShip(ship, board);
     }
   }
-  return { positionShip, getRandomInt, rows, buildArmada, placeShip, placeArmada, checkIfOnBoard, checkForDupes, addPositions }
+  
+  return { positionShip, getRandomInt, rows, buildArmada, placeShip, placeArmada, checkIfOnBoard, checkForDupes }
 }
 
 export { boardFactory } 

95b555dd0d22a204edfc2b2c0e7e52279ed500f5
MissLiner <miss.liner@yahoo.com>
2022-01-28 21:05:31 -0500

    Fix tests to reflect module changes

--- src/tests/makeBoard.test.js
+++ src/tests/makeBoard.test.js
@@ -20,60 +20,17 @@ describe('makeBoard tests', () => {
 })
 
 describe('ship placement tests', () => {
-  const testBoard3 = makeBoard.boardFactory();
+  const testBoard2 = makeBoard.boardFactory();
 
   test('places ship on board', () => {
   const testHorPositions = [ { row: 2, column: 6 }, { row: 2, column: 7 }];
-  testBoard3.placeShip(testHorPositions);
-  expect(testBoard3.rows[2]).toStrictEqual(['open', 'open', 'open', 'open', 'open', 'open', 'ship', 'ship', 'open', 'open']);
+  testBoard2.placeShip(testHorPositions);
+  expect(testBoard2.rows[2]).toStrictEqual(['open', 'open', 'open', 'open', 'open', 'open', 'ship', 'ship', 'open', 'open']);
   })
 })
 
-describe('armada tests', () => { 
-  const testBoard2 = makeBoard.boardFactory();
-  testBoard2.buildArmada('testPlayer');
-  testBoard2.placeArmada();
-
-  test('armada is an array of objects', () => {
-    expect(testBoard2.getArmada()[2]).toStrictEqual(expect.any(Object));
-  })
-  test('armada array.length is 5', () => {
-    expect(testBoard2.getArmada().length).toBe(5);
-  })
-  test('armada has 17 positions', () => {
-    expect(testBoard2.getAllShipPositions(testBoard2.getArmada()).length).toBe(17);
-  })
-  test('reports 83 open spaces after armada placement', () => {
-    expect(testBoard2.findOpenSpaces().length).toBe(83);
-  })
-  test('armada ship hit() works', () => {
-    testBoard2.getArmada()[1].hit();
-    expect(testBoard2.getArmada()[1].getHits()).toBe(1);
-  })
-  test('board sends hit to ship',() => {
-    testBoard2.hitShip(testBoard2.getArmada()[2].getPositions()[0]);
-    expect(testBoard2.getArmada()[2].getHits()).toBe(1);
-  })
-  test('ship doesn\'t sink on first hit',() => {
-    testBoard2.hitShip(testBoard2.getArmada()[2].getPositions()[0]);
-    expect(testBoard2.getArmada()[2].getStatus()).toBe('afloat');
-  })
-  test('board sinks ship', () => {
-    testBoard2.hitShip(testBoard2.getArmada()[0].getPositions()[0]);
-    testBoard2.hitShip(testBoard2.getArmada()[0].getPositions()[1]);
-    expect(testBoard2.getArmada()[0].getStatus()).toBe('sunk');
-  })
-  test('armada sinks', () => {
-    const testLengthArray = [ 1, 2, 3, 4 ];
-    let testCounter = 10;
-    expect(testBoard2.checkIfAllSunk(testLengthArray, testCounter)).toBe('sunk');
-  })
-  test('dupe check - with dupes', () => {
-    const testArr = [{ "row": 1, "column": 2 }, { "row": 2, "column": 3}, { "row": 1, "column": 2 }];
-      expect(testBoard2.checkForDupes(testArr)).toBe(true);
-  })
-  test('dupe check - no dupes', () => {
-    const testArr = [{ "row": 4, "column": 2}, { "row": 2, "column": 3}, { "row": 1, "column": 2 }];
-      expect(testBoard2.checkForDupes(testArr)).toBe(false);
-  })
+test('armada sinks', () => {
+  const testLengthArray = [ 1, 2, 3, 4 ];
+  let testCounter = 10;
+  expect(testBoard2.checkIfAllSunk(testLengthArray, testCounter)).toBe('sunk');
 })
--- src/tests/makePlayer.test.js
+++ src/tests/makePlayer.test.js
@@ -1,8 +1,45 @@
 const makePlayer = require('../modules/makePlayer');
 const makeBoard = require('../modules/makeBoard')
 
-// test('AI makes guess', () => {
-//   const testBoard = makeBoard.boardFactory();
-//   const testPlayer = makePlayer.playerFactory(true, 2);
-//   expect(testPlayer.makeGuess(testBoard)).
-// })
+describe('armada tests', () => { 
+  const oppBoard = makeBoard.boardFactory();
+  const testAIPlayer = makePlayer.playerFactory('Hal', true, oppBoard);
+
+  test('armada is an array of objects', () => {
+    expect(testAIPlayer.getArmada()[2]).toStrictEqual(expect.any(Object));
+  })
+  test('armada array.length is 5', () => {
+    expect(testAIPlayer.getArmada().length).toBe(5);
+  })
+  test('armada has 17 positions', () => {
+    expect(testAIPlayer.getAllShipPositions(testAIPlayer.getArmada()).length).toBe(17);
+  })
+  test('reports 83 open spaces after armada placement', () => {
+    expect(testAIPlayer.findOpenSpaces().length).toBe(83);
+  })
+  test('armada ship hit() works', () => {
+    testAIPlayer.getArmada()[1].hit();
+    expect(testAIPlayer.getArmada()[1].getHits()).toBe(1);
+  })
+  test('board sends hit to ship',() => {
+    oppBoard.hitShip(testAIPlayer.getArmada()[2].getPositions()[0]);
+    expect(testAIPlayer.getArmada()[2].getHits()).toBe(1);
+  })
+  test('ship doesn\'t sink on first hit',() => {
+    oppBoard.hitShip(testAIPlayer.getArmada()[2].getPositions()[0]);
+    expect(testAIPlayer.getArmada()[2].getStatus()).toBe('afloat');
+  })
+  test('board sinks ship', () => {
+    oppBoard.hitShip(testAIPlayer.getArmada()[0].getPositions()[0]);
+    oppBoard.hitShip(testAIPlayer.getArmada()[0].getPositions()[1]);
+    expect(testAIPlayer.getArmada()[0].getStatus()).toBe('sunk');
+  })
+
+  test('dupe check - with dupes', () => {
+    const testArr = [{ "row": 1, "column": 2 }, { "row": 2, "column": 3}, { "row": 1, "column": 2 }];
+      expect(testAIPlayer.checkForDupes(testArr)).toBe(true);
+  })
+  test('dupe check - no dupes', () => {
+    const testArr = [{ "row": 4, "column": 2}, { "row": 2, "column": 3}, { "row": 1, "column": 2 }];
+      expect(testAIPlayer.checkForDupes(testArr)).toBe(false);
+  })
